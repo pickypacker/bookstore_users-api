e8a73c3e6cd657650cf7de7103117477affa3746
Picky Packer <pickypacker@gmail.com>
2021-06-19 16:41:37 +0200

    2021-06-19 16:41

--- app/mapping_url.go
+++ app/mapping_url.go
@@ -6,7 +6,7 @@ func mapURLs() {
 
 	router.GET("/", controllers.Root)
 	router.GET("/ping", controllers.Ping)
-	router.GET("/users/user_id", controllers.GetUser)
+	router.GET("/users/:user_id", controllers.GetUser)
 	router.GET("/users/search", controllers.SearchUser)
 	router.POST("/users", controllers.CreateUser)
 }
--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -1,17 +1,20 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func GetUser(c *gin.Context) {
 
+	c.String(http.StatusNotImplemented, "Not implemented")
 }
 
 func CreateUser(c *gin.Context) {
-
+	c.String(http.StatusNotImplemented, "Not implemented")
 }
 
 func SearchUser(c *gin.Context) {
-
+	c.String(http.StatusNotImplemented, "Not implemented")
 }
