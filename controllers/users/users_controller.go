fdbc8c8c466309c34736943befe40d88196ef1f5
Picky Packer <pickypacker@gmail.com>
2021-06-20 08:57:05 +0200

    2021-06-20 08:55

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,9 +1,13 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pickypacker/bookstore_users-api/domain/users"
 )
 
 func GetUser(c *gin.Context) {
@@ -12,7 +16,18 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Not implemented")
+	var user users.User
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+
+	if err == nil {
+
+		err = json.Unmarshal(bytes, &user)
+		if err == nil {
+
+		} else {
+			fmt.Println(err)
+		}
+	}
 }
 
 func SearchUser(c *gin.Context) {
--- domain/users/user.go
+++ domain/users/user.go
@@ -1,1 +1,9 @@
-package user
+package users
+
+type User struct {
+	Id          int64  `json:"id"`
+	FristName   string `1json:"first_name"`
+	LastName    string `json:"lst_name"`
+	Email       string `json:"email"`
+	DateCreated string `json:"date_created"`
+}
